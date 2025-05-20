7956cbf5bad8bb9f10ff3765b3041aa6590a4292
Krzysztof Fonał <krzysiekfonal@gmail.com>
2025-05-20 13:08:49 +0200

    Test change for vercel ci 2

--- src/env.ts
+++ src/env.ts
@@ -4,7 +4,7 @@ import { z } from 'zod'
 export const env = createEnv({
   clientPrefix: 'NEXT_PUBLIC_',
   client: {
-    NEXT_PUBLIC_GOLEM_BASE_RPC_URL: z.string().url().default('http://localhost:8545'),
+    NEXT_PUBLIC_GOLEM_BASE_RPC_URL: z.string().url().default('http://localhost:8545/'),
   },
   runtimeEnvStrict: {
     NEXT_PUBLIC_GOLEM_BASE_RPC_URL: process.env.NEXT_PUBLIC_GOLEM_BASE_RPC_URL,
